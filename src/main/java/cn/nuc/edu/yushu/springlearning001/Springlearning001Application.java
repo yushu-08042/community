ae386d0dffad722078a12b0ed9ce4870ce152104
yushu-08042 <43470815+yushu-08042@users.noreply.github.com>
2019-12-03 19:44:28 +0800

    init repo

---
+++ src/main/java/cn/nuc/edu/yushu/springlearning001/Springlearning001Application.java
@@ -0,0 +1,13 @@
+package cn.nuc.edu.yushu.springlearning001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlearning001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springlearning001Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/nuc/edu/yushu/springlearning001/Springlearning001ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nuc.edu.yushu.springlearning001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlearning001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
